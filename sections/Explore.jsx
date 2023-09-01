5ecf480b3b5bf068a293a6aa03fb38994df5eb90
teanliu <teanliuu@gmail.com>
2023-09-02 01:22:42 +0800

    feat: update About.jsx

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,5 +1,13 @@
 'use client';
 
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { TypingText, ExploreCard, TitleText } from "../components";
+
 const Explore = () => (
   <section>
     Explore section
